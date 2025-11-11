23ae6c34ead750eebf84fb83ccca6539ca69e37e
emma-cambridge <emma.cambridge1@education.nsw.gov.au>
2025-11-11 12:31:23 +1100

    Created player movement and started scripting the camera

--- Rollaball/Assets/Scripts/PlayerController.cs
+++ Rollaball/Assets/Scripts/PlayerController.cs
@@ -3,6 +3,8 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 0;
+
     private Rigidbody rb;
     private float movementX;
     private float movementY;
@@ -13,14 +15,19 @@ public class PlayerController : MonoBehaviour
         rb = GetComponent<Rigidbody>();
     }
 
-    void OnMove(Inputvalue movementValue)
+    void OnMove(InputValue movementValue)
     {
         Vector2 movementVector = movementValue.Get<Vector2>();
-        
-    }
 
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+    
     private void FixedUpdate()
     {
-        rb.AddForce(movementVector);
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+
+        rb.AddForce(movement * speed);
     }
+   
 }
